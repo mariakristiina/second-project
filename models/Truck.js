1582bf64c9de08eec5f453933c8f1f605f661c33
Alexandra Servie <alexservie@Alexandras-MacBook-Pro-2.local>
2019-11-15 16:45:02 +0100

    model updated

--- models/Truck.js
+++ models/Truck.js
@@ -1,8 +1,21 @@
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 const truckSchema = new Schema({
   name: String,
-  cuisine: [‘Latin American’, ‘Italian’, ‘German’, ‘Mediterranean’, ‘Vietnamese’, ‘Chinese’, ‘American’, ‘alcohol’],
-  tags: [‘vegetarian’, ‘vegan’, ‘barbeque’, ‘burger’, ‘asian’, ‘dumplings’, ‘pizza’, ‘dessert’, ]
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+  cuisine: {
+    type: String,
+    enum: ["Latin American", "Italian", "German", "Mediterranean", "Vietnamese", "Chinese", "American", "alcohol"],
+    required: true
+  }
+  tags: {
+    type: String,
+    enum: ["vegetarian", "vegan", "barbeque", "burger", "asian", "dumplings", "pizza", "dessert"],
+  }
 });
-const Truck = mongoose.model(‘Truck’, truckSchema);
+const Truck = mongoose.model("Truck", truckSchema);
 module.exports = Truck;
