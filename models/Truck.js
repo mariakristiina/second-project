851a955c422ef86e538434aecef3497cc575697e
Maria Palonen <mariakristiina@Marias-MacBook-Pro.local>
2019-11-15 16:45:55 +0100

    changes

--- models/Truck.js
+++ models/Truck.js
@@ -1,8 +1,8 @@
 const Schema = mongoose.Schema;
 const truckSchema = new Schema({
   name: String,
-  cuisine: [‘Latin American’, ‘Italian’, ‘German’, ‘Mediterranean’, ‘Vietnamese’, ‘Chinese’, ‘American’, ‘alcohol’],
+  cuisine: ["Latin American", "Italian", 'German', ‘Mediterranean’, ‘Vietnamese’, ‘Chinese’, ‘American’, ‘alcohol’],
   tags: [‘vegetarian’, ‘vegan’, ‘barbeque’, ‘burger’, ‘asian’, ‘dumplings’, ‘pizza’, ‘dessert’, ]
 });
-const Truck = mongoose.model(‘Truck’, truckSchema);
+const Truck = mongoose.model("Truck", truckSchema);
 module.exports = Truck;
